f7c7e64a9c85748d073edb39642c32a2220ae607
zhaolei <zhaolei0791@126.com>
2021-06-30 21:31:42 +0800

    docs: udpate package.json

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,25 @@
-declare const atom: (initValue: any) => {
-    value: any;
-    updaters: Map<any, any>;
-    update: (value: any) => void;
-};
+import React from 'react';
 
-declare const useAtom: (atomInstance: any) => [any, (value: any) => void];
+interface Atom<Value> {
+    value: Value;
+    updaters: Map<any, boolean>;
+    update: (value: Value) => void;
+}
+declare type AtomCreator<Value> = (key: symbol) => Atom<Value>;
+declare const atom: <Value>(initValue: Value) => AtomCreator<Value>;
 
-export { atom, useAtom };
+declare const useAtom: <Value>(atomCreator: AtomCreator<Value>) => [Value, (value: Value) => void];
+
+declare const selectAtom: <Value, Slice>(atomCreator: AtomCreator<Value>, selector: (value: Value) => Slice, equalityFn?: ((pre: Slice, next: Slice) => boolean) | undefined) => AtomCreator<Slice>;
+
+declare const Context: React.Context<symbol | null>;
+/**
+ * A Provider is a component that contains a store and provides atom values under the component tree.
+ * A Provider works just like React context provider. If you don't use a Provider, it works as
+ * provider-less mode with a default store. A Provider will be necessary if we need to hold different
+ * atom values for different component trees. Provider also has some capabilities described below,
+ * which doesn't exist in the provider-less mode.
+ */
+declare const Provider: React.FC;
+
+export { Atom, AtomCreator, Context, Provider, atom, selectAtom, useAtom };
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
-import{useState as t,useEffect as e}from"react";var r=function(t){var e={value:t,updaters:new Map,update:function(t){e.value!==t&&(e.value=t,e.updaters.forEach((function(t,e){return e((function(t){return!t}))})))}};return e},u=function(r){var u=t(!1)[1];return e((function(){return r.updaters.set(u,!0),function(){r.updaters.delete(u)}}),[r]),[r.value,r.update]};export{r as atom,u as useAtom};
+import t,{useMemo as r,useState as n,useContext as e,useEffect as u}from"react";import{jsx as a}from"react/jsx-runtime";var o=function(t){var r=new Map;return function(n){if(!r.get(n)){var e={value:t,updaters:new Map,update:function(t){e.value!==t&&(e.value=t,e.updaters.forEach((function(t,r){return r((function(t){return!t}))})))}};r.set(n,e)}return r.get(n)}},c=function(){return(c=Object.assign||function(t){for(var r,n=1,e=arguments.length;n<e;n++)for(var u in r=arguments[n])Object.prototype.hasOwnProperty.call(r,u)&&(t[u]=r[u]);return t}).apply(this,arguments)},i=t.createContext(null),f=function(t){var n=r((function(){return Symbol("atoms-react")}),[]);return a(i.Provider,c({value:n},{children:t.children}),void 0)},l=Symbol("atoms-react"),p=function(t){var a=n(!1)[1],o=e(i)||l,c=r((function(){return t(o)}),[o]);return u((function(){return c.updaters.set(a,!0),function(){c.updaters.delete(a)}}),[c]),[c.value,c.update]},v=function(t,r,n){return function(e){var u=t(e),a={value:r(u.value),updaters:new Map,update:function(t){}};return u.updaters.set((function(){var t=r(u.value);n?n(a.value,t)||a.updaters.forEach((function(t,r){return r((function(t){return!t}))})):a.value!==t&&a.updaters.forEach((function(t,r){return r((function(t){return!t}))})),a.value=t}),!0),a}};
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */export{i as Context,f as Provider,o as atom,v as selectAtom,p as useAtom};
 //# sourceMappingURL=index.js.map
